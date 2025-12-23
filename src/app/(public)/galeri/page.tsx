fcb220197455bee4e62388cd60d05912f92a223e
fauzyDev <haliffauzy45@gmail.com>
2025-12-23 19:22:12 +0700

    refactor: page galeri

--- src/app/(public)/galeri/page.tsx
+++ src/app/(public)/galeri/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Route } from "next"
+import Image from "next/image";
+import Link from "next/link";
+import { Route } from "next";
+import { Card, CardHeader, CardContent } from '@/components/ui/card';
 
 const image = [
   {
@@ -35,17 +36,19 @@ export default function page() {
       <h1 className='text-3xl text-center text-gray-900 font-bold'>Galeri Kegiatan</h1>
       <div className="grid md:grid-cols-4 gap-6 p-4">
         {image.map((index) => (
-          <div key={index.id} className="border-3 border-gray-300 rounded-xl p-4 bg-card shadow-md overflow-hidden hover:shadow-lg transition">
+          <Card key={index.id} className="border-3 border-gray-300 rounded-xl p-4 bg-card shadow-md overflow-hidden hover:shadow-lg transition">
             <Image src={index.url} alt="galeri" height={400} width={600} sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw,25vw" className="w-full h-auto" />
-            <div className="p-4">
+            <CardHeader className="p-4">
               <h3 className="text-xl font-semibold">{index.title}</h3>
-            </div>
-            <Link
-              href={`/kegiatan/${index.slug}` as Route}
-              className="text-primary text-center font-semibold hover:underline bg-white shadow-md">
-              Selengkapnya →
-            </Link>
-          </div>
+            </CardHeader>
+            <CardContent>
+              <Link
+                href={`/kegiatan/${index.slug}` as Route}
+                className="text-primary text-center font-semibold hover:underline bg-white shadow-md">
+                Selengkapnya →
+              </Link>
+            </CardContent>
+          </Card>
         ))}
       </div>
     </div>
